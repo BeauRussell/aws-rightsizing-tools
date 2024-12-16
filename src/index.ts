ac139ec6053d27e3506bbe85f999b7713094eb4c
BeauRussell <yourknightmares@gmail.com>
2024-12-16 13:14:42 -0600

    Pull all regions

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,24 @@
 import { listLambdas } from "./lambda/index.js";
+import { FunctionConfiguration } from "@aws-sdk/client-lambda";
 
-listLambdas("us-east-2").then((lambdas) => {
-	console.log(lambdas);
-});
+const regionList: string[] = [
+	"us-east-1",
+	"us-east-2",
+	"us-west-1",
+	"us-west-2",
+	"eu-central-1",
+	"eu-west-1",
+];
+
+function main() {
+	const promiseList: Promise<FunctionConfiguration[]>[] = [];
+	for const region of regionList {
+		promiseList.push(listLambdas(region));
+	}
+
+	Promises.all(promiseList).then((results) => {
+		console.log("Results: ", results);
+	}
+}
+
+main();
