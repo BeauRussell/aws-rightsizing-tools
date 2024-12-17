d4f22ec6e4bede41b93852cc7c008189fb9ca95e
BeauRussell <yourknightmares@gmail.com>
2024-12-16 20:05:05 -0600

    Test Using Query Instead

--- src/cloudwatch/index.ts
+++ src/cloudwatch/index.ts
@@ -1,35 +1,38 @@
-import { CloudWatchLogsClient, GetLogEventsCommand, DescribeLogStreamsCommand, DescribeLogStreamsCommandOutput, LogStream, GetLogEventsCommandOutput } from "@aws-sdk/client-cloudwatch-logs";
+import { CloudWatchLogsClient, GetQueryResultsCommandInput, GetQueryResultsCommand, GetQueryResultsCommandOutput, StartQueryCommandInput, StartQueryCommand, StartQueryCommandOutput } from "@aws-sdk/client-cloudwatch-logs";
 import { strict as assert } from "node:assert";
 
 async function getLogEvents(region: string, logGroup: string | undefined): Promise<void> {
 	assert(region, 'region is required');
 	assert(logGroup, 'logGroup is required');
 	const client = new CloudWatchLogsClient({ region: region });
-	let nextToken: string | undefined = undefined;
-		do {
-		const getStreamCommand = new DescribeLogStreamsCommand({ logGroupName: logGroup, nextToken });
-		let getStreamsResponse: DescribeLogStreamsCommandOutput;
-		let logStreamNames: string[] = [];
-		try {
-			getStreamsResponse = await client.send(getStreamCommand);
-			if (getStreamsResponse.logStreams) {
-				logStreamNames.push(...getStreamsResponse.logStreams.map((stream: LogStream) => stream.logStreamName) as string[]);
-			}
-		} catch (err) {
-			throw new Error(`Failed to get log streams from log group ${logGroup}, error: ${err}`);
+	
+	const query: string = "filter @message LIKE /Max Memory Used:/";
+	const params: StartQueryCommandInput = {
+		logGroupName: logGroup,
+		queryString: query,
+		endTime: new Date().getTime(),
+		startTime: new Date().getTime() - 1000 * 60 * 60 * 24 * 30,
+		limit: 1000,
+	};
+
+	let response: StartQueryCommandOutput;
+	try {
+		const command = new StartQueryCommand(params);
+		response = await client.send(command);
+		
+		const queryId: string | undefined = response.queryId;
+
+		if (queryId) {
+			let results: GetQueryResultsCommandOutput;
+			const resultsParams: GetQueryResultsCommandInput = {
+				queryId: queryId,
+			};
+			results  = await client.send(new GetQueryResultsCommand(resultsParams));
+			console.log(results);
 		}
-
-		if (logStreamNames.length === 0) {
-			throw new Error(`No log streams found in log group ${logGroup}`);
-		}
-
-		const stream: LogStream = getStreamsResponse.logStreams![0];
-		const getEventsCommand = new GetLogEventsCommand({ logGroupName: logGroup, logStreamName: stream.logStreamName });
-		const getEventsResponse: GetLogEventsCommandOutput = await client.send(getEventsCommand);
-
-		console.log(getEventsResponse);
-
-	} while (nextToken);
+	} catch (err) {
+		console.error(err);
+	}
 }
 
 export { getLogEvents };
