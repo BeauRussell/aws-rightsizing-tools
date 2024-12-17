252f2bb2ee2f2dad569a84fa3b03644977f1a56c
BeauRussell <yourknightmares@gmail.com>
2024-12-17 07:40:46 -0600

    Add memory use calculation

--- src/cloudwatch/index.ts
+++ src/cloudwatch/index.ts
@@ -1,4 +1,4 @@
-import { CloudWatchLogsClient, GetQueryResultsCommandInput, GetQueryResultsCommand, GetQueryResultsCommandOutput, StartQueryCommandInput, StartQueryCommand, StartQueryCommandOutput, ResourceNotFoundException } from "@aws-sdk/client-cloudwatch-logs";
+import { CloudWatchLogsClient, GetQueryResultsCommandInput, GetQueryResultsCommand, GetQueryResultsCommandOutput, StartQueryCommandInput, StartQueryCommand, StartQueryCommandOutput, ResourceNotFoundException, ResultField } from "@aws-sdk/client-cloudwatch-logs";
 import { strict as assert } from "node:assert";
 
 async function getLogEvents(region: string, logGroup: string | undefined): Promise<void> {
@@ -23,19 +23,9 @@ async function getLogEvents(region: string, logGroup: string | undefined): Promi
 		const queryId: string | undefined = response.queryId;
 
 		if (queryId) {
+			console.log(logGroup);
 			setTimeout(async () => {
-				let results: GetQueryResultsCommandOutput;
-				const resultsParams: GetQueryResultsCommandInput = {
-					queryId: queryId,
-				};
-				results  = await client.send(new GetQueryResultsCommand(resultsParams));
-				if (results.results && results.results!.length > 0) {
-					console.log(results.results);
-					console.log(results.results[0]);
-					console.log(results.results[0][0]);
-					console.log(results.results[0][0].value);
-					console.log(results.results[0][0].field);
-				}
+				await processQuery(queryId, client);
 			}, 60000);
 		}
 	} catch (err) {
@@ -44,6 +34,49 @@ async function getLogEvents(region: string, logGroup: string | undefined): Promi
 		} else {
 			console.error(err);
 		}
+	}
+}
+
+async function processQuery(queryId: string, client: CloudWatchLogsClient): Promise<void> {
+	let results: GetQueryResultsCommandOutput;
+	const resultsParams: GetQueryResultsCommandInput = {
+		queryId: queryId,
+	};
+	results = await client.send(new GetQueryResultsCommand(resultsParams));
+	if (results.results && results.results!.length > 0) {
+		processMessages(results.results);
+	}
+}
+
+function processMessages(results: ResultField[][]): void {
+	let maxMemoryUsedCount: number = 0;
+	let messageCount: number = 0;
+	for (const result of results) {
+		for (const field of result) {
+			if (field.field === "@message") {
+				messageCount++;
+				if (checkMemoryMaxed(field.value)) {
+					maxMemoryUsedCount++;
+				}
+			}
+		}
+	}
+
+	console.log(`Messages Processed: ${messageCount}`);
+	console.log(`Max Memory Used Messages: ${maxMemoryUsedCount}`);
+}
+
+function checkMemoryMaxed(message: string): boolean {
+	const regex = /Memory Size: ([0-9]*)|Max Memory Used: ([0-9]*)/
+	const match = message.match(regex);
+	if (match && match.length > 0) {
+		const maxMemoryUsed = parseInt(match[2]);
+		const memorySize = parseInt(match[1]);
+		if (maxMemoryUsed >= memorySize) {
+			return true;
+		}
+	} else {
+		return false;
 	}
 }
 
